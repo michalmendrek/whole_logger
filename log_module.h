e7203e97784622cbabbcd82ef877df2c2e277d62
Michal <michal.mendrek@intive.com>
2019-09-17 13:03:29 +0200

    Moved parts to mod_loger.h to private

--- log_module.h
+++ log_module.h
@@ -10,12 +10,13 @@ class log_module {
  private:
   std::vector<std::string> LinksToLog;
   std::string PathToLogDir;
-  // std::vector<log_loger *> Loger;
+  std::vector<log_loger *> Loger;
+  notify *Notify;
  public:
   log_module(std::string Path);
   ~log_module();
-  notify *Notify;
-  std::vector<log_loger *> Loger;
+  //notify *Notify;
+  //std::vector<log_loger *> Loger;
   void AddModule(std::string ModuleName, std::function<void(std::string,std::string)> CallBack);
   std::string GetWholeLog(std::string ModuleName);
 };
--- main.cpp
+++ main.cpp
@@ -3,17 +3,22 @@
 
 using namespace std;
 
-void funkcja_testu(std::string data,std::string name) { std::cout << "ModName: " << name << "Data: " << data << std::endl; }
+void funkcja_testu(std::string data, std::string name) {
+  std::cout << "ModName: " << name << "Data: " << data << std::endl;
+}
 
-void funkcja_dwa(std::string data, std::string name) { std::cout << "Dwa" << std::endl;  std::cout << "ModName: " << name << "Data: " << data << std::endl;}
+void funkcja_dwa(std::string data, std::string name) {
+  std::cout << "Dwa" << std::endl;
+  std::cout << "ModName: " << name << "Data: " << data << std::endl;
+}
 
 int main() {
   log_module plik("./");
-  plik.AddModule("module1",funkcja_testu);
-  plik.AddModule("module2",funkcja_dwa);
+  plik.AddModule("module1", funkcja_testu);
+  plik.AddModule("module2", funkcja_dwa);
 
- std::cout << "Module1 " <<  plik.GetWholeLog("module1") << std::endl;
- std::cout << "Module2 " <<  plik.GetWholeLog("module2") << std::endl;
+  std::cout << "Module1 " << plik.GetWholeLog("module1") << std::endl;
+  std::cout << "Module2 " << plik.GetWholeLog("module2") << std::endl;
   while (1)
     ;
 }
